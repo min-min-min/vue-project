1a28d0d1284e9b08dde10f7e802febab867f816b
chenjing <1134991001@qq.com>
2017-05-08 11:40:56 +0800

    import bootstrap assets

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -23,7 +23,8 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src')
+      '@': resolve('src'),
+      'assets':path.resolve(__dirname, '../src/assets')
     }
   },
   module: {
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,12 +1,64 @@
 <template>
-  <div class="hello">
+  <div>
+    <nav class="navbar navbar-default" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                  data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">{{ msg }}</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <h1>{{ msg }}</h1>
-    <h1>{{ msg2 }}</h1>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
     </ul>
-    <button id="btn1">click</button>
+    <button id="btn1" class="btn btn-default">click</button>
+    <input class="form-control" v-model="msg2"/>
+    <h1>{{ msg2 }}</h1>
   </div>
 </template>
 
@@ -15,16 +67,20 @@
     name: 'test',
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App',
-        msg2: 'sajgdjksadasd'
+        msg: '这个页面是用来测试各种组件的',
+        msg2: ''
       }
     }
   }
-$(document).ready(function () {
-  $("#btn1").click(function () {
-    alert("Sadad")
+
+  $(document).ready(function () {
+    $("#btn1").click(function () {
+      alert("Sadad")
+    })
+    $("#btn1").mouseover(function () {
+      console.log("asdasdsadasfdfgdfgdfg")
+    })
   })
-})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
